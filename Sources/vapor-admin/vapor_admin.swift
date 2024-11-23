59636dc19db31f2e2053f35be4f307078865b300
Jimmy W. Hough Jr <jimmyhoughjr@me.com>
2024-11-23 09:42:47 -0600

    A required refactor as we need to be around at the consumers compile time to have any hoope of using macros to consume its models for UI

---
+++ Sources/vapor-admin/vapor_admin.swift
@@ -0,0 +1,9 @@
+import Vapor
+
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+public struct VaporAdmin {
+    public static func banner() -> String {
+        "Hello Vapor Admin!"
+    }
+}
