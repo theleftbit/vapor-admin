7f6f3756f4c692aa170dbb7a375dc88260c9f851
Jimmy W. Hough Jr <jimmyhoughjr@me.com>
2024-11-27 11:04:04 -0600

    ....

--- Sources/VaporAdmin/standup.sh
+++
@@ -1,5 +0,0 @@
-##!/bin/bash
-echo "${GREEN}Building app  ${NC}"
-docker compose up db redis&
-swift run App serve --env development
-
--- Tests/vapor-adminTests/vapor_adminTests.swift
+++
@@ -1,6 +0,0 @@
-import Testing
-@testable import vapor_admin
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
